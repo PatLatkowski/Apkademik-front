fe46251d610da99536a13a74099228304cd74e41
haniakrajnik98 <haniakrajnik98@gmail.com>
2020-08-05 11:19:33 +0200

    Design of register page.

--- src/index.js
+++ src/index.js
@@ -1,38 +1,18 @@
 import ReactDOM from "react-dom";
 import React from "react";
-import logo from "./components/logo.png";
-import "./index.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+import Login from "./login";
+import Register from "./register";
 
 class App extends React.Component {
   render() {
     return (
-      <div className="container">
-        <div className="topBox">
-          <div className="logo">
-            <img src={logo} />
-          </div>
-          <div className="loginInput">
-            <form id="form">
-              <input type="text" id="login" placeholder="Login" />
-              <br />
-              <br />
-              <input type="password" id="password" placeholder="Password" />
-            </form>
-          </div>
-        </div>
-        <div className="bottomBox">
-          <button type="submit" name="Submit">
-            Login
-          </button>
-          <br />
-          <br />
-          <button type="submit" name="Register">
-            Register
-          </button>
-        </div>
-      </div>
+      <Router>
+        <Route path="/login" component={Login} />
+        <Route path="/register" component={Register} />
+      </Router>
     );
   }
 }
-
 ReactDOM.render(<App />, document.getElementById("root"));
