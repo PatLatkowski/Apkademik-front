9d0d31d7ea6ecd27852de5a3463d7a3db6c2088e
Jan Kwarciński <uruael@gmail.com>
2020-09-06 19:28:55 +0200

    Schedule scroll added

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -59,7 +59,7 @@ function Schedule() {
       case rstate.RESERVED:
         break;
       case rstate.FREE:
-        reservationColor2[i] = "lightgray";
+        reservationColor2[i] = "#25e81f";
         setReservationColor(reservationColor2);
         reservationState2[i] = rstate.HOVER;
         setReservationState(reservationState2);
@@ -99,22 +99,28 @@ function Schedule() {
 
   function generateTable() {
     var result = [];
+    result.push(<div class="row sticky-top">{generateDays()}</div>);
     for (var i = 0; i < numOfHours; i++) {
-      result.push(<div class="row">{createHours(i)}</div>);
+      result.push(
+        <div class="row">
+          {generateHours(i)}
+          {createHours(i)}
+        </div>
+      );
     }
     return result;
   }
 
-  function generateHours() {
+  function generateHours(i) {
     var result = [];
     var startHour = 7;
-    for (var i = 0; i < numOfHours; i++) {
-      result.push(
-        <div calss="row" className="hour">
-          {startHour + i}:00
-        </div>
-      );
-    }
+    //for (var i = 0; i < numOfHours; i++) {
+    result.push(
+      <div calss="col" className="hour">
+        {startHour + i}:00
+      </div>
+    );
+    // }
     return result;
   }
 
@@ -127,6 +133,14 @@ function Schedule() {
 
   function generateDays() {
     var result = [];
+    result.push(
+      <div calss="col" className="hour">
+        GMT{" "}
+        {selectedDate.getTimezoneOffset() / 60 > 0
+          ? "-" + selectedDate.getTimezoneOffset() / 60
+          : "+" + (selectedDate.getTimezoneOffset() / 60) * -1}
+      </div>
+    );
     for (var i = 0; i < 5; i++) {
       result.push(
         <div calss="col" className="date">
@@ -140,16 +154,8 @@ function Schedule() {
   return (
     <div class="wrapper" className="schedule-box">
       <div class="row">
-        <div class="col-1"></div>
-        <div class="col">
-          <div class="row">{generateDays()}</div>
-        </div>
-        <div class="col-2"></div>
-      </div>
-      <div class="row">
-        <div class="col-1">{generateHours()}</div>
-        <div class="col">{generateTable()}</div>
-        <div class="col-2">
+        <div class="col scroll">{generateTable()}</div>
+        <div class="col-2 rightcol">
           <div class="row">
             <div class="col mx-auto">
               <MuiPickersUtilsProvider utils={DateFnsUtils}>
