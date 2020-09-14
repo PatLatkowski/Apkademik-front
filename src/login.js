3527d87f49d0a54da9763a0e8d5ed9922a056fc0
Patryk.Lat <58081819+TrashPanda77@users.noreply.github.com>
2020-09-14 21:45:58 +0200

    Changed login.js from class to hook

--- src/login.js
+++ src/login.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./components/logo.png";
 import Joi from "@hapi/joi";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import "./CSS/login.css";
 import ErrorMessage from "./components/ErrorMessage";
 import Cookies from "universal-cookie";
 import { UserContext } from "./index";
+import { useInput } from "./components/useInput";
 
 const serverUrl = "http://46.41.142.44:8080";
 
@@ -17,113 +18,102 @@ var schema = Joi.object().keys({
   password: Joi.string().min(8).required(),
 });
 
-class Login extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { login: "" };
-    this.state = { password: "" };
-    this.state = { errorMessage: "" };
-    this.handleChange = this.handleChange.bind(this);
-    this.handleLogin = this.handleLogin.bind(this);
-  }
+function Login(props) {
+  let history = useHistory();
+  const [errorMessage, setErrorMessage] = useState("");
+  const { value: login, bind: loginChange, reset: resetLogin } = useInput("");
+  const {
+    value: password,
+    bind: passwordChange,
+    reset: resetPassword,
+  } = useInput("");
 
-  handleChange(event) {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleLogin(event) {
+  const handleLogin = (event) => {
+    event.preventDefault();
     schema.validate(
       {
-        login: this.state.login,
-        password: this.state.password,
+        login: login,
+        password: password,
       },
       (err, res) => {
         if (err) {
-          this.setState({ errorMessage: err.details[0].message });
+          setErrorMessage(err.details[0].message);
           console.log(err);
         } else {
           axios
             .post(serverUrl + "/authenticate", {
-              email: this.state.login,
-              password: this.state.password,
+              email: login,
+              password: password,
             })
             .then((response) => {
               const cookies = new Cookies();
               cookies.set("token", response.data["token"], { path: "/" });
               console.log(cookies);
-              this.props.history.push("/");
+              history.push("/");
             })
             .catch((error) => {
               if (error.request.status === 401)
-                this.setState({ errorMessage: "Incorrect email or password" });
+                setErrorMessage("Incorrect email or password");
               console.log(error);
             });
           console.log("Ok");
         }
       }
     );
+    resetLogin();
+    resetPassword();
+  };
 
-    event.preventDefault();
-  }
-
-  render() {
-    return (
-      <div className="container login-container">
-        <div className="row m-2">
-          <div className="col-6">
-            <img src={logo} alt="logo" className="rounded float-right" />
-          </div>
-          <div className="col-3 login-inputDiv">
-            <form id="log" onSubmit={this.handleLogin}>
-              <div className="row form-group">
-                <input
-                  type="text"
-                  id="login"
-                  placeholder="email"
-                  name="login"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                />
-              </div>
-              <div className="row form-group">
-                <input
-                  type="password"
-                  id="password"
-                  placeholder="Password"
-                  name="password"
-                  value={this.state.value}
-                  onChange={this.handleChange}
-                />
-              </div>
-            </form>
-            <div className="row">
-              <ErrorMessage text={this.state.errorMessage} />
+  return (
+    <div className="container login-container">
+      <div className="row m-2">
+        <div className="col-6">
+          <img src={logo} alt="logo" className="rounded float-right" />
+        </div>
+        <div className="col-3 login-inputDiv">
+          <form id="log" onSubmit={handleLogin}>
+            <div className="row form-group">
+              <input
+                type="text"
+                id="login"
+                placeholder="email"
+                name="login"
+                {...loginChange}
+              />
             </div>
-          </div>
-        </div>
-        <div className="row form-group">
-          <div className="col-5 mx-auto">
-            <input
-              form="log"
-              type="submit"
-              name="Submit"
-              value="Submit"
-              className="login-input"
-            />
-          </div>
-        </div>
-        <div className="row form-group">
-          <div className="col-5 mx-auto login-button">
-            <Link to="/register">
-              <button type="submit" name="Register">
-                Register
-              </button>
-            </Link>
+            <div className="row form-group">
+              <input
+                type="password"
+                id="password"
+                placeholder="Password"
+                name="password"
+                {...passwordChange}
+              />
+            </div>
+          </form>
+          <div className="row">
+            <ErrorMessage text={errorMessage} />
           </div>
         </div>
       </div>
-    );
-  }
+      <div className="row form-group">
+        <div className="col-5 mx-auto">
+          <button type="submit" form="log" className="login-input">
+            Submit
+          </button>
+        </div>
+      </div>
+      <div className="row form-group">
+        <div className="col-5 mx-auto login-button">
+          <Link to="/register">
+            <button type="submit" name="Register">
+              Register
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
