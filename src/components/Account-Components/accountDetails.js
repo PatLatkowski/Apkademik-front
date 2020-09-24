525be47e00ce3f8e9f6ad7be6455a326e3981129
Patryk.Lat <58081819+TrashPanda77@users.noreply.github.com>
2020-09-24 12:04:26 +0200

    AccountDetails update - GET Dorm Name and Room Number

--- src/components/Account-Components/accountDetails.js
+++ src/components/Account-Components/accountDetails.js
@@ -1,17 +1,25 @@
-import React, { useState, forwardRef, useImperativeHandle } from "react";
+import React, {
+  useState,
+  forwardRef,
+  useImperativeHandle,
+  useEffect,
+} from "react";
 import axios from "axios";
 import Cookies from "universal-cookie";
+import { serverUrl } from "../../consts";
 
 const AccountDetails = forwardRef((props, ref) => {
   const [name, setName] = useState("null");
   const [surname, setSurname] = useState("null");
   const [email, setEmail] = useState("null");
-  const [dormNum, setDormNum] = useState("1");
-  const [roomNum, setRoomNum] = useState("1");
+  const [dormNum, setDormNum] = useState("0");
+  const [roomNum, setRoomNum] = useState("0");
 
   useImperativeHandle(ref, () => ({
     refreshData() {
       getAccountInfo();
+      getUserDorm();
+      getUserRoom();
     },
   }));
 
@@ -23,7 +31,7 @@ const AccountDetails = forwardRef((props, ref) => {
     };
     try {
       axios
-        .get("http://46.41.142.44:8080/user", config)
+        .get(serverUrl + "/user", config)
         .then((response) => {
           setName(response.data.name);
           setSurname(response.data.surname);
@@ -34,14 +42,57 @@ const AccountDetails = forwardRef((props, ref) => {
           console.log(error);
         });
     } catch (e) {
-      const {
-        response: { data },
-      } = e;
       console.log(e);
     }
   }
 
-  getAccountInfo();
+  function getUserDorm() {
+    const cookies = new Cookies();
+    const token = cookies.get("token");
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    try {
+      axios
+        .get(serverUrl + "/dorm", config)
+        .then((response) => {
+          console.log(response);
+          setDormNum(response.data.name);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  function getUserRoom() {
+    const cookies = new Cookies();
+    const token = cookies.get("token");
+    const config = {
+      headers: { Authorization: `Bearer ${token}` },
+    };
+    try {
+      axios
+        .get(serverUrl + "/room", config)
+        .then((response) => {
+          console.log(response);
+          setRoomNum(response.data.number);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } catch (e) {
+      console.log(e);
+    }
+  }
+
+  useEffect(() => {
+    getAccountInfo();
+    getUserDorm();
+    getUserRoom();
+  }, []);
 
   return (
     <div className="elementContainer">
@@ -54,7 +105,7 @@ const AccountDetails = forwardRef((props, ref) => {
           <div className="w-100">Email:</div>
           <div className="w-100">Name:</div>
           <div className="w-100">Surname:</div>
-          <div className="w-100">Dorm Number:</div>
+          <div className="w-100">Dorm Name:</div>
           <div className="w-100">Room Number:</div>
         </div>
         <div className="col">
